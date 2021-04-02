d7f5340cf1df04ebc55688a993a22ad7e35d81db
Tiago92869 <73955757+Tiago92869@users.noreply.github.com>
2021-04-02 17:02:49 +0100

    Update SubscriptionDelete.java
    tem o erro agora

--- src/SubscriptionDelete.java
+++ src/SubscriptionDelete.java
@@ -14,62 +14,61 @@ import models.Subscription;
 public class SubscriptionDelete extends HttpServlet {
 String ida;
 String idc;
-int p;
+int c;
+int a;
 int i;
 JSONObject obj=new JSONObject();
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Subscription s=new Subscription();
 	       ida=request.getParameter("id_activity");
-	       p=Integer.parseInt(ida);
-	       s.setId_activity(p);
+	       a=Integer.parseInt(ida);
+	       s.setId_activity(a);
 	       idc=request.getParameter("id_child");
-	       p=Integer.parseInt(idc);
-	       s.setId_child(p);
+	       c=Integer.parseInt(idc);
+	       s.setId_child(c);
 	       
-	       
+	     
+
 	       
 	       SubscriptionDeleteIMP SDI=new SubscriptionDeleteIMP();
-	       i=SDI.SDelete(s);
-	       
-	       if(i>0) {
+			JSONObject MSG=new JSONObject();
+	       i=SDI.SDelete(s); 
+	       if(i==1) {
 	    	  try {
-				obj.put("STATUS","200");
+	    		  MSG.put("STATUS","200");
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} 
-	    	   
-	    	   
+			}else {
+				
+				
+				try {
+					MSG.put("STATUS","400" );
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
+
 	    	   
 	    	   
 	    	   
 	       }
 	       
 	       
+	       
+	       
+	       
 	       PrintWriter out = response.getWriter();
 	       response.setContentType("application/json");
 	       response.setCharacterEncoding("UTF-8");
-	       out.print(obj);
+	       out.print(MSG);
 	       out.flush();   
 		
 		
 		
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	
 }
