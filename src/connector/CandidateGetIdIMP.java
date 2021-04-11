26b696c4f1eab50fb5cfc362e7c6c5c553489478
Tiago92869 <73955757+Tiago92869@users.noreply.github.com>
2021-04-11 16:29:56 +0100

    Add files via upload

---
+++ src/connector/CandidateGetIdIMP.java
@@ -0,0 +1,64 @@
+
+package connector;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Candidate_Institution;
+
+public class CandidateGetIdIMP {
+	String DRIVER="com.mysql.jdbc.Driver";
+	String URL="jdbc:mysql://localhost:3306";
+	String USER="BD";
+	String PASSWORD="12341234";
+	String sql="Select * from dai.candidate_institution where id_candidate = ?";
+	ResultSet rs;
+	List<Candidate_Institution> lista=new ArrayList<Candidate_Institution>();
+	Candidate_Institution o=new Candidate_Institution();
+	public List<Candidate_Institution> Get(Candidate_Institution cand) {
+		// TODO Auto-generated method stub
+		  int id_candidate=cand.getId_candidate();
+			
+			//Connection to database and store value in
+					try {
+						Class.forName(DRIVER);
+						Connection con=DriverManager.getConnection(URL,USER,PASSWORD);
+						PreparedStatement ps=con.prepareStatement(sql);
+						ps.setInt(1,id_candidate);
+						ps.setString(2, "closed");
+				            rs=ps.executeQuery();
+				           while( rs.next()) {
+			            	int nif =rs.getInt("nif");
+			            	String name=rs.getString("name");
+			            	String address=rs.getString("address");
+			            	String county=rs.getString("county");
+			            	String district=rs.getString("district");
+			            	String post_code=rs.getString("post_code");
+			            	String email=rs.getString("email");
+			            	int phone_number=rs.getInt("phone_number");
+				          
+				            lista.add(new Candidate_Institution(id_candidate, name, address, county, district, post_code, nif, phone_number, email));
+				  
+				           }
+						
+					} catch (SQLException | ClassNotFoundException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+						
+					}
+					return lista;
+					
+				}
+		
+		
+		
+		
+
+}
