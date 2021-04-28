256935f5a1614743ace8b99bb74c29b9aa317e57
Tiago92869 <73955757+Tiago92869@users.noreply.github.com>
2021-04-28 20:54:43 +0100

    Add files via upload

---
+++ src/CollaboratorUserJOINGet.java
@@ -0,0 +1,71 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import connector.CollaboratorUserJOINGetIMP;
+import models.CollaboratorUserJOIN;
+
+public class CollaboratorUserJOINGet extends HttpServlet {
+
+	
+	JSONObject l=new JSONObject();	
+	List<CollaboratorUserJOIN> o;	
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	        
+		CollaboratorUserJOINGetIMP RGAI=new CollaboratorUserJOINGetIMP();
+	        o=RGAI.RewardsGetAllMethod();
+	        
+	        
+	        try {
+				l.put("MSG", o);
+			} catch (JSONException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			
+			 PrintWriter out = response.getWriter();
+			    response.setContentType("application/json");
+			    response.setCharacterEncoding("UTF-8");
+			    try {
+					out.print(l.get("MSG"));
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			    out.flush();   
+	        
+	        
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+	
+	
+	
+	
+	
+	
+}
