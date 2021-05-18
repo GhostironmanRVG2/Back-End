217654239667ee77e76ff79982551dfed3537dcb
Tiago92869 <73955757+Tiago92869@users.noreply.github.com>
2021-05-18 01:41:55 +0100

    Update CollaboratorPutByCollaboratorIMP.java

--- src/connector/CollaboratorPutByCollaboratorIMP.java
+++ src/connector/CollaboratorPutByCollaboratorIMP.java
@@ -10,15 +10,15 @@ import models.Collaborator;
 
 public class CollaboratorPutByCollaboratorIMP {
 	String DRIVER="com.mysql.jdbc.Driver";
-	String URL="jdbc:mysql://localhost:3306";
-	String USER="BD";
-	String PASSWORD="12341234";
-String sql="Update dai.collaborator set name=?,type=?,address=?,county=?,district=?,post_code=?,nif=?,phone_number=?,id_user=? where id_collaborator=? ";
+	String URL="jdbc:mysql://188.82.156.135:3306";
+	String USER="monkey";
+	String PASSWORD="monkey";
+String sql="Update dai.collaborator c, dai.user u set c.name=?,c.address=?,c.county=?,c.district=?,c.post_code=?,c.nif=?,c.phone_number=?, u.email=?,c.id_user=? where c.id_user = u.id_user and c.id_collaborator=? ";
 int i;
 	public int Put(Collaborator c) {
 		// extract
 	   String name = c.getName();
-	   String type = c.getType();
+	   String email = c.getEmail();
 	   String address = c.getAddress();
 	   String county = c.getCounty();
 	   String district = c.getDistrict();
@@ -34,13 +34,13 @@ int i;
 			Connection con=DriverManager.getConnection(URL,USER,PASSWORD);
 			PreparedStatement ps=con.prepareStatement(sql);
 			ps.setString(1 , name);
-			ps.setString(2 , type);
-			ps.setString(3 , address);
-			ps.setString(4 , county);
-			ps.setString(5 , district);
-			ps.setString(6 , post_code);
-			ps.setInt(7 , nif);
-			ps.setInt(8 , phone_number);
+			ps.setString(2 , address);
+			ps.setString(3 , county);
+			ps.setString(4 , district);
+			ps.setString(5 , post_code);
+			ps.setInt(6 , nif);
+			ps.setInt(7 , phone_number);
+			ps.setString(8 , email);
 			ps.setInt(9 , id_user);
 			ps.setInt(10 , id_collaborator);
 			i=ps.executeUpdate();
